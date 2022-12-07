f95c0b1f89406a90c0760767288e80d4ac7d1efb
DARSHAN JAIN <108194698+Darshanjain2507@users.noreply.github.com>
2022-12-08 01:02:07 +0530

    Create Basic Calculator.py

---
+++ Basic Calculator.py
@@ -0,0 +1,12 @@
+//Create a calculator capable of performing addition, subtraction, division and multiplication operation of two numbers.
+
+
+
+#*******************CALCULATOR********************
+
+a=50
+b=3
+print("The value of", a,"+" ,b , "is" ,a+b)
+print("The value of", a,"-" ,b , "is" ,a-b)
+print("The value of", a,"/" ,b , "is" ,a/b)
+print("The value of", a,"*" ,b , "is" ,a*b)
